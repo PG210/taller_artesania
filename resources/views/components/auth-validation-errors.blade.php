7d03030f300d9d0247b14ee10104d5e10b2ae4ea
PG210 <pedrocuasquer21095@gmail.com>
2021-01-19 01:16:02 -0500

    agrega mensaje

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -2,8 +2,8 @@
 
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
-            {{ __('Whoops! Something went wrong.') }}
+        <div class="font-medium text-blue-600">
+            {{ __('Oops! Su contraseña es incorrecta') }}
         </div>
 
         <ul class="mt-3 list-disc list-inside text-sm text-red-600">
